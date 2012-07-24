741778e0a9f2eb338927ce11eaabc7dcefaebc93
Tsutomu Kawamura <kawamura@cognitom.com>
2012-07-25 04:34:31 +0900

    動的にCSSに画像を埋め込む(data-uri)ように変更。

--- style/embed-images.php
+++ style/embed-images.php
@@ -1,12 +1,12 @@
 <?php
-$original = '_images.css';
-$dest = 'images.css';
-
-$css = file_get_contents($original);
+$file = $_SERVER['DOCUMENT_ROOT'] . str_replace('?inline', '', $_SERVER['REQUEST_URI']);
+$css = file_get_contents($file);
 $css = preg_replace_callback('/url\((.*?)\)/im', function($matches){
 	$url = trim($matches[1], '\'\"');
 	return file_exists($url)
 		? 'url(data:image/png;base64,' . base64_encode(file_get_contents($url)) . ')'
 		: '';
 }, $css);
-file_put_contents($dest, $css);
+
+header("Content-type: text/css");
+echo $css;

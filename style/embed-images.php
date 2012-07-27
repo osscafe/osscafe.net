15ff56d62949908e2052cca178a1f1355bf1edfc
Tsutomu Kawamura <kawamura@cognitom.com>
2012-07-28 07:42:34 +0900

    CSSに限定して、embed_images.phpに直接アクセスを禁ず。対応する埋込ファイルの拡張子をpng,jpg,gifに限定する。

--- style/embed-images.php
+++ style/embed-images.php
@@ -1,12 +1,13 @@
 <?php
-$file = $_SERVER['DOCUMENT_ROOT'] . str_replace('?inline', '', $_SERVER['REQUEST_URI']);
-$css = file_get_contents($file);
-$css = preg_replace_callback('/url\((.*?)\)/im', function($matches){
-	$url = trim($matches[1], '\'\"');
-	return file_exists($url)
-		? 'url(data:image/png;base64,' . base64_encode(file_get_contents($url)) . ')'
-		: '';
-}, $css);
-
-header("Content-type: text/css");
-echo $css;
+$mime_types = array('png'=>'image/png', 'jpg'=>'image/jpeg', 'gif'=>'image/gif');
+header('Content-type: text/css');
+echo preg_replace_callback(
+	'/url\((.*?\.(' . implode('|', array_keys($mime_types)) . '))\)/im',
+	function ($matches) use ($mime_types) {
+		$url = trim($matches[1], '\'\"');
+		return file_exists($url)
+			? 'url(data:' . $mime_types[$matches[2]] . ';base64,' . base64_encode(file_get_contents($url)) . ')'
+			: $matches[0];
+	},
+	file_get_contents($_SERVER['DOCUMENT_ROOT'] . str_replace('?inline', '', $_SERVER['REQUEST_URI']))
+);

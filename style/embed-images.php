aeb1facfb4ef7da9b0873192e28fda1accccd8c6
Tsutomu Kawamura <kawamura@cognitom.com>
2012-07-28 07:46:36 +0900

    Herokuでの.htaccessの挙動テスト

--- style/embed-images.php
+++ style/embed-images.php
@@ -6,7 +6,7 @@ echo preg_replace_callback(
 	function ($matches) use ($mime_types) {
 		$url = trim($matches[1], '\'\"');
 		return file_exists($url)
-			? 'url(data:' . $mime_types[$matches[2]] . ';base64,' . base64_encode(file_get_contents($url)) . ')'
+			? 'url(data:' . $mime_types[strtolower($matches[2])] . ';base64,' . base64_encode(file_get_contents($url)) . ')'
 			: $matches[0];
 	},
 	file_get_contents($_SERVER['DOCUMENT_ROOT'] . str_replace('?inline', '', $_SERVER['REQUEST_URI']))

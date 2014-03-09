03f086882f3f18fff70f74cd500b73252ac1e3ce
Tsutomu Kawamura <kawamura@cognitom.com>
2014-03-09 15:37:37 +0900

    For debugging.

--- api/index.php
+++ api/index.php
@@ -12,7 +12,7 @@ $facebook = new Facebook(array(
 
 $app->get('/fb/event/debug.json', function () use ($facebook) {
   echo date_default_timezone_get();
-  echo date('M j', '2014-03-12T10:00:00+0900');
+  echo date('M j', strtotime('2014-03-12T10:00:00+0900'));
 });
 
 /**
